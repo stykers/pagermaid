c99307990109a6a2e6f723bedff5fc629a0052b3
Katherine ❄ <stykers@stykers.moe>
2019-06-14 12:19:12 +0900

    environment based logging configuration, and python version check

---
+++ jarvis/__init__.py
@@ -0,0 +1,35 @@
+""" Jarvis initialization. """
+
+import os
+
+from sys import version_info
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient
+import redis
+
+
+load_dotenv("config.env")
+
+# logging stuff
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=INFO
+    )
+LOGS = getLogger(__name__)
+
+if version_info[0] < 3 or version_info[1] < 6:
+    LOGS.error(
+        "Please upgrade your python interpreter."
+    )
+    exit(1)
