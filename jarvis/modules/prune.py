ee7dc075aa48e77c0493c56924cfca51e020056b
Stykers <stykers@stykers.moe>
2019-07-12 09:15:23 +0900

    code consistency update for prune

--- jarvis/modules/prune.py
+++ jarvis/modules/prune.py
@@ -6,33 +6,33 @@ from jarvis import log, log_chatid, command_help
 from jarvis.events import register
 
 
-@register(outgoing=True, pattern="^.prune$")
-async def prune(message):
+@register(outgoing=True, pattern="^-prune$")
+async def prune(context):
     """ Purge every single message after the message you replied to. """
-    if not message.text[0].isalpha() and message.text[0] not in ("/", "#", "@", "!"):
-        chat = await message.get_input_chat()
+    if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
+        chat = await context.get_input_chat()
         msgs = []
         count = 0
 
-        async for msg in message.client.iter_messages(chat, min_id=message.reply_to_msg_id):
+        async for msg in context.client.iter_messages(chat, min_id=context.reply_to_msg_id):
             msgs.append(msg)
             count = count + 1
-            msgs.append(message.reply_to_msg_id)
+            msgs.append(context.reply_to_msg_id)
             if len(msgs) == 100:
-                await message.client.delete_messages(chat, msgs)
+                await context.client.delete_messages(chat, msgs)
                 msgs = []
 
         if msgs:
-            await message.client.delete_messages(chat, msgs)
-        notify = await message.client.send_message(
-            message.chat_id,
+            await context.client.delete_messages(chat, msgs)
+        notify = await context.client.send_message(
+            context.chat_id,
             "Deleted "
             + str(count)
             + " messages.",
         )
 
         if log:
-            await message.client.send_message(
+            await context.client.send_message(
                 log_chatid, "Deleted " +
                 str(count) + " messages."
             )
