77b56b503fe7a7b4612838274b48c6573ab21f01
Stykers <stykers@stykers.moe>
2019-07-12 11:29:11 +0900

    some error handling for prune, and help strings

--- jarvis/modules/prune.py
+++ jarvis/modules/prune.py
@@ -9,64 +9,72 @@ from jarvis.events import register
 @register(outgoing=True, pattern="^-prune$")
 async def prune(context):
     """ Purge every single message after the message you replied to. """
-    if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
-        chat = await context.get_input_chat()
-        msgs = []
-        count = 0
+    try:
+        if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
+            chat = await context.get_input_chat()
+            msgs = []
+            count = 0
 
-        async for msg in context.client.iter_messages(chat, min_id=context.reply_to_msg_id):
-            msgs.append(msg)
-            count = count + 1
-            msgs.append(context.reply_to_msg_id)
-            if len(msgs) == 100:
+            async for msg in context.client.iter_messages(chat, min_id=context.reply_to_msg_id):
+                msgs.append(msg)
+                count = count + 1
+                msgs.append(context.reply_to_msg_id)
+                if len(msgs) == 100:
+                    await context.client.delete_messages(chat, msgs)
+                    msgs = []
+
+            if msgs:
                 await context.client.delete_messages(chat, msgs)
-                msgs = []
-
-        if msgs:
-            await context.client.delete_messages(chat, msgs)
-        notify = await context.client.send_message(
-            context.chat_id,
-            "Deleted "
-            + str(count)
-            + " messages.",
-        )
-
-        if log:
-            await context.client.send_message(
-                log_chatid, "Deleted " +
-                            str(count) + " messages."
+            notify = await context.client.send_message(
+                context.chat_id,
+                "Deleted "
+                + str(count)
+                + " messages.",
             )
-        await sleep(3)
-        await notify.delete()
+
+            if log:
+                await context.client.send_message(
+                    log_chatid, "Deleted " +
+                                str(count) + " messages."
+                )
+            await sleep(3)
+            await notify.delete()
+    except TypeError:
+        await context.edit("`Please reply to a message.`")
 
 
 @register(outgoing=True, pattern="^-selfprune")
 async def selfprune(context):
     """ Prune self message. """
     if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
-        message = context.text
-        count = int(message[11:])
-        i = 1
+        try:
+            message = context.text
+            count = int(message[11:])
+            i = 1
 
-        async for message in context.client.iter_messages(context.chat_id, from_user='me'):
-            if i > count + 1:
-                break
-            i = i + 1
-            await message.delete()
+            async for message in context.client.iter_messages(context.chat_id, from_user='me'):
+                if i > count + 1:
+                    break
+                i = i + 1
+                await message.delete()
 
-        notification = await context.client.send_message(
-            context.chat_id,
-            "Deleted "
-            + str(count)
-            + " messages.",
-        )
-        if log:
-            await context.client.send_message(
-                log_chatid, "Deleted " +
-                            str(count) + " messages."
+            notification = await context.client.send_message(
+                context.chat_id,
+                "Deleted "
+                + str(count)
+                + " messages.",
             )
-        await sleep(3)
-        await notification.delete()
+            if log:
+                await context.client.send_message(
+                    log_chatid, "Deleted " +
+                                str(count) + " messages."
+                )
+            await sleep(3)
+            await notification.delete()
+        except ValueError:
+            await context.edit("`Invalid parameter.`")
+        except:
+            await context.edit("`An error occurred while the server is interpreting this command.`")
 
 
 @register(outgoing=True, pattern="^-delete$")
@@ -125,4 +133,31 @@ async def timed(context):
             if log:
                 await context.client.send_message(log_chatid, "Created timed message.")
         except ValueError:
-            await context.edit("`Invalid time parameter specified.`")
+            await context.edit("`Invalid parameter.`")
+        except:
+            await context.edit("`An error occurred while the server is interpreting this command.`")
+
+command_help.update({
+    "prune": "Parameter: -prune\
+    \nUsage: Deletes everything starting from the message you replied to."
+})
+
+command_help.update({
+    "selfprune": "Parameter: -selfprune <integer>\
+    \nUsage: Deletes your own messages."
+})
+
+command_help.update({
+    "delete": "Parameter: -delete\
+    \nUsage: Deletes the message you reply to."
+})
+
+command_help.update({
+    "edit": "Parameter: -edit <string>\
+    \nUsage: Edits your last message."
+})
+
+command_help.update({
+    "timed": "Parameter: -timed <integer> <string>\
+    \nUsage: Generate messages that deletes itself."
+})
