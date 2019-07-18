4df541cd76ea97ed6fd62417d3cc000aa59d2037
Stykers <stykers@stykers.moe>
2019-07-18 13:02:39 +0900

    shell, with the gorgeous zsh theme

--- jarvis/modules/system.py
+++ jarvis/modules/system.py
@@ -1,13 +1,73 @@
 """ System related utilities for Jarvis to integrate into the system. """
 
 import asyncio
+import platform
 
 from asyncio.subprocess import PIPE
 from getpass import getuser
 from os import remove
+from os import geteuid
 from sys import executable
 from jarvis import command_help, log, log_chatid
 from jarvis.events import register
+
+
+@register(outgoing=True, pattern="^-sh(?: |$)(.*)")
+async def sh(context):
+    """ For calling a binary from the shell. """
+    if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
+        user = getuser()
+        command = context.pattern_match.group(1)
+        uid = geteuid()
+        hostname = platform.node()
+        if context.is_channel and not context.is_group:
+            await context.edit("`Current configuration disables shell execution in channel.`")
+            return
+
+        if not command:
+            await context.edit("`Invalid argument.`")
+            return
+
+        process = await asyncio.create_subprocess_shell(
+            command,
+            stdout=PIPE,
+            stderr=PIPE
+        )
+        stdout, stderr = await process.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        if len(result) > 4096:
+            output = open("output.txt", "w+")
+            output.write(result)
+            output.close()
+            await context.client.send_file(
+                context.chat_id,
+                "output.log",
+                reply_to=context.id,
+                caption="`Output exceeded limit, attaching file.`",
+            )
+            remove("output.txt")
+            return
+
+        if uid is 0:
+            await context.edit(
+                f"`{user}`@{hostname} ~"
+                f"\n> `#` {command}"
+                f"\n`{result}`"
+            )
+        else:
+            await context.edit(
+                f"`{user}`@{hostname} ~"
+                f"\n> `$` {command}"
+                f"\n`{result}`"
+            )
+
+        if log:
+            await context.client.send_message(
+                log_chatid,
+                "Command `" + command + "` executed.",
+            )
 
 
 @register(outgoing=True, pattern="^-pip(?: |$)(.*)")
@@ -55,7 +115,7 @@ async def pip(context):
                     "`\n**Output: **\n`No output.`"
                 )
         else:
-            await context.edit("`Invalid argument, check module help.`")
+            await context.edit("`Invalid argument.`")
 
 
 command_help.update({
