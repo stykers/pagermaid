5d7a22c8b6bc029245c70c57a30bcfe7494afbf0
Katherine ❄ <stykers@stykers.moe>
2019-07-26 10:54:58 +0900

    implement regex

---
+++ jarvis/modules/regex.py
@@ -0,0 +1,102 @@
+""" Jarvis regex utility. """
+
+import re
+
+from sre_constants import error as sre_err
+from jarvis.events import register
+
+deliminators = ("/", ":", "|", "_")
+
+
+@register(outgoing=True, pattern="^s")
+async def sed(command):
+    """ Implements regex on telegram. """
+    result = format_sed(command.text)
+    target = await command.get_reply_message()
+    if result:
+        if target:
+            message = target.text
+        else:
+            await command.edit(
+                "`Unable to retrieve target data.`"
+            )
+            return
+
+        reply, reply_result, flags = result
+
+        if not reply:
+            await command.edit(
+                "`Unable to retrieve target data.`"
+            )
+            return
+
+        try:
+            check = re.match(reply, message, flags=re.IGNORECASE)
+            if check and check.group(0).lower() == message.lower():
+                await command.edit(
+                    "`Invalid argument.`"
+                )
+                return
+
+            if "i" in flags and "g" in flags:
+                text = re.sub(reply, reply_result, message, flags=re.I).strip()
+            elif "i" in flags:
+                text = re.sub(reply, reply_result, message,
+                              count=1, flags=re.I).strip()
+            elif "g" in flags:
+                text = re.sub(reply, reply_result, message).strip()
+            else:
+                text = re.sub(reply, reply_result, message, count=1).strip()
+        except sre_err:
+            await command.edit("`Syntax error in regex.`")
+            return
+        if text:
+            await command.edit(text)
+
+
+def format_sed(data):
+    """ Separate sed arguments. """
+    if (
+            len(data) >= 1 and
+            data[1] in deliminators and
+            data.count(data[1]) >= 2
+    ):
+        target = data[1]
+        start = counter = 2
+        while counter < len(data):
+            if data[counter] == "\\":
+                counter += 1
+
+            elif data[counter] == target:
+                replace = data[start:counter]
+                counter += 1
+                start = counter
+                break
+
+            counter += 1
+
+        else:
+            return None
+
+        while counter < len(data):
+            if (
+                    data[counter] == "\\" and
+                    counter + 1 < len(data) and
+                    data[counter + 1] == target
+            ):
+                data = data[:counter] + data[counter + 1:]
+
+            elif data[counter] == target:
+                replace_with = data[start:counter]
+                counter += 1
+                break
+
+            counter += 1
+        else:
+            return replace, data[start:], ""
+
+        flags = ""
+        if counter < len(data):
+            flags = data[counter:]
+        return replace, replace_with, flags.lower()
+    return None
