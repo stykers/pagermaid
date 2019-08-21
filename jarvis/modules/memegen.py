a749ca045f88c99c4be5e79790f165596510fa51
Katherine ❄ <stykers@stykers.moe>
2019-08-21 12:43:31 +0900

    fix path of meme gen

--- jarvis/modules/memegen.py
+++ jarvis/modules/memegen.py
@@ -7,7 +7,7 @@ from jarvis import command_help, log, log_chatid
 from jarvis.events import register
 
 
-@register(outgoing=True, pattern=r"^-meme(?: |$)([\s\S]*)")
+@register(outgoing=True, pattern="^-meme(?: |$)(.*)")
 async def meme(context):
     """ Generates the meme. """
     if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
@@ -19,6 +19,7 @@ async def meme(context):
         target_file_path = await context.client.download_media(
             await context.get_reply_message()
         )
+        await context.edit("`Generating meme, please wait . . .`")
         command = "./utils/meme.sh \"" + \
                   target_file_path + \
                   "\" meme.png" + \
@@ -37,35 +38,18 @@ async def meme(context):
         stdout, stderr = await execute.communicate()
         result = str(stdout.decode().strip()) \
             + str(stderr.decode().strip())
-        try:
-            try:
-                await context.client.send_file(
-                    context.chat_id,
-                    "meme.png",
-                    reply_to=reply_id
-                )
-                await context.delete()
-                success = True
-                message = string_1 + "` and `" + string_2
-            except IndexError:
-                await context.edit("`Target is not a valid image.`")
-                success = False
-                message = None
-            remove(target_file_path)
-            remove("meme.png")
-        except AttributeError:
-            await context.edit("`Invalid argument.`")
-            return
-        if success:
-            if log:
-                await context.client.send_message(
-                    log_chatid, "Meme generated with text `" + message + "`."
-                )
-        else:
-            if log:
-                await context.client.send_message(
-                    log_chatid, "Attempted to generate meme without success."
-                )
+        await context.client.send_file(
+            context.chat_id,
+            "meme.png",
+            reply_to=reply_id
+        )
+        await context.delete()
+        success = True
+        message = string_1 + "` and `" + string_2
+        if log:
+            await context.client.send_message(
+                log_chatid, "Meme generated with text `" + message + "`."
+            )
 command_help.update({
     "meme": "Parameter: -meme <image> <text>\
     \nUsage: Generates a meme with specified image and text."
