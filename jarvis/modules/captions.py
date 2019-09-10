3a9fea505f697074aa366585314f67f9d08d170f
Katherine ❄ <stykers@stykers.moe>
2019-09-10 17:05:51 +0900

    native tesseract based OCR

--- jarvis/modules/captions.py
+++ jarvis/modules/captions.py
@@ -64,3 +64,41 @@ command_help.update({
     "caption": "Parameter: -caption <text>,<text> <image>\
     \nUsage: Adds two lines of captions to an image."
 })
+
+
+@register(outgoing=True, pattern="^-ocr(?: |$)(.*)")
+@diagnostics
+async def ocr(context):
+    """ Extracts texts from images. """
+    if not context.text[0].isalpha() and context.text[0] not in ("/", "#", "@", "!"):
+        if context.fwd_from:
+            return
+        reply = await context.get_reply_message()
+        await context.edit("`Processing image, please wait . . .`")
+        if reply:
+            target_file_path = await context.client.download_media(
+                await context.get_reply_message()
+            )
+        else:
+            target_file_path = await context.download_media()
+        if target_file_path is None:
+            await context.edit("`There are no attachment in target.`")
+            return
+        result = await execute(f"tesseract {target_file_path} stdout")
+        if not result:
+            await context.edit("`Something wrong happened, please report this problem.`")
+            try:
+                remove(target_file_path)
+            except FileNotFoundError:
+                pass
+            return
+        await context.edit(result)
+        remove(target_file_path)
+        if log:
+            await context.client.send_message(
+                log_chatid, "Performed OCR on an image."
+            )
+command_help.update({
+    "ocr": "Parameter: -ocr <image>\
+    \nUsage: Extracts texts from images."
+})
