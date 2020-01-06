edc70730904a2f6fe20edeff3ec62baa0a68acc1
KAT#ΦωΦ <stykers@stykers.moe>
2020-01-06 18:23:05 +0900

    merge ghost toggle command

--- pagermaid/modules/ghost.py
+++ pagermaid/modules/ghost.py
@@ -4,37 +4,32 @@ from pagermaid import redis, redis_check, command_help, log, log_chatid
 from pagermaid.events import register, diagnostics
 
 
-@register(outgoing=True, pattern="^-ghost_enable$")
+@register(outgoing=True, pattern="^^-ghost(?: |$)(.*)")
 @diagnostics
 async def ghost_enable(context):
-    """ Enables ghosting of a user. """
+    """ Toggles ghosting of a user. """
     if not redis_check():
         await context.edit("Redis is offline, cannot operate.")
         return
-    redis.set("ghosted.chat_id." + str(context.chat_id), "true")
-    await context.delete()
-    if log:
-        await context.client.send_message(
-            log_chatid,
-            "UserID " + str(context.chat_id) + " added to list of ghosted users.")
-
-
-@register(outgoing=True, pattern="^-ghost_disable$")
-@diagnostics
-async def ghost_disable(context):
-    """ Disables ghosting of a user. """
-    if not redis_check():
-        await context.edit("Redis is offline, cannot operate.")
-        return
-    redis.delete("ghosted.chat_id." + str(context.chat_id))
-    await context.delete()
-    if log:
-        await context.client.send_message(
-            log_chatid,
-            "UserID " + str(context.chat_id) + " removed from list of ghosted users.")
+    if context.pattern_match.group(1) == 'true':
+        redis.set("ghosted.chat_id." + str(context.chat_id), "true")
+        await context.delete()
+        if log:
+            await context.client.send_message(
+                log_chatid,
+                "UserID " + str(context.chat_id) + " added to list of ghosted users.")
+    elif context.pattern_match.group(1) == 'false':
+        redis.delete("ghosted.chat_id." + str(context.chat_id))
+        await context.delete()
+        if log:
+            await context.client.send_message(
+                log_chatid,
+                "UserID " + str(context.chat_id) + " removed from list of ghosted users.")
+    else:
+        await context.edit("Invalid argument.")
 command_help.update({
-    "ghost_disable": "Parameter: -ghost_disable\
-    \nUsage: Disables ghosting of a user."
+    "ghost": "Parameter: -ghost <true|false>\
+    \nUsage: Toggles ghosting of user, requires redis."
 })
 
 
