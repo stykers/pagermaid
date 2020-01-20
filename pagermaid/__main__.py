234a810c88c3059fd9588be5d9f1e6fcf9f4fe71
KAT#ΦωΦ <stykers@stykers.moe>
2020-01-20 23:06:29 +0900

    fix plugin importing

--- pagermaid/__main__.py
+++ pagermaid/__main__.py
@@ -5,7 +5,7 @@ from os import getcwd
 from importlib import import_module
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from pagermaid import logs, bot
-from pagermaid.modules import module_list
+from pagermaid.modules import module_list, plugin_list
 
 invalid_phone = '\nInvalid phone number entered.' \
                 '\nPlease make sure you specified' \
@@ -23,5 +23,10 @@ for module_name in module_list:
         imported_module = import_module("pagermaid.modules." + module_name)
     except BaseException:
         logs.info(f"Unable to load module {module_name}.")
+for plugin_name in plugin_list:
+    try:
+        imported_plugin = import_module("plugins." + plugin_name)
+    except BaseException:
+        logs.info(f"Unable to load plugin {plugin_name}.")
 logs.info("PagerMaid have started, The prefix is -, type -help for help message.")
 bot.run_until_disconnected()
--- pagermaid/modules/__init__.py
+++ pagermaid/modules/__init__.py
@@ -8,17 +8,28 @@ from pagermaid import logs
 
 def __list_modules():
     module_paths = glob(dirname(__file__) + "/*.py")
+    result = [
+        basename(file)[:-3]
+        for file in module_paths
+        if isfile(file) and file.endswith(".py") and not file.endswith("__init__.py")
+    ]
+    return result
+
+
+def __list_plugins():
     plugin_paths = glob(f"{getcwd()}/plugins" + "/*.py")
     if not exists(f"{getcwd()}/plugins"):
         makedirs(f"{getcwd()}/plugins")
     result = [
         basename(file)[:-3]
-        for file in module_paths or plugin_paths
+        for file in plugin_paths
         if isfile(file) and file.endswith(".py") and not file.endswith("__init__.py")
     ]
     return result
 
 
 module_list = sorted(__list_modules())
+plugin_list = sorted(__list_plugins())
 logs.info("Loading modules: %s", str(module_list))
+logs.info("Loading plugins: %s", str(plugin_list))
 __all__ = module_list + ["module_list"]
