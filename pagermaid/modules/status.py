b12c75a4b37db51c861960af257391d210f33ae0
KAT#ΦωΦ <stykers@stykers.moe>
2020-01-25 16:22:42 +0900

    moved topcloud rendering to it's class

--- pagermaid/modules/status.py
+++ pagermaid/modules/status.py
@@ -1,16 +1,18 @@
-""" Module that show system info of the hardware the bot is running on. """
+""" PagerMaid module that contains utilities related to system status. """
 
-from os import remove
+from os import remove, popen
 from datetime import datetime
 from speedtest import Speedtest
 from telethon import functions
 from platform import python_version, uname
-from shutil import which
+from wordcloud import WordCloud
 from telethon import version as telethon_version
 from sys import platform
-from pagermaid import log
+from re import sub
+from pathlib import Path
+from pagermaid import log, config
 from pagermaid.listener import listener
-from pagermaid.utils import unit_convert, execute, make_top_cloud, redis_check
+from pagermaid.utils import unit_convert, execute, redis_check
 
 
 @listener(outgoing=True, command="sysinfo",
@@ -146,7 +148,61 @@ async def topcloud(context):
     """ Generates a word cloud of resource-hungry processes. """
     if context.fwd_from:
         return
-    await make_top_cloud(context)
+    await context.edit("Generating image . . .")
+    command_list = []
+    if not Path('/usr/bin/top').is_symlink():
+        output = str(await execute("top -b -n 1")).split("\n")[7:]
+    else:
+        output = str(await execute("top -b -n 1")).split("\n")[4:]
+    for line in output[:-1]:
+        line = sub(r'\s+', ' ', line).strip()
+        fields = line.split(" ")
+        try:
+            if fields[11].count("/") > 0:
+                command = fields[11].split("/")[0]
+            else:
+                command = fields[11]
+
+            cpu = float(fields[8].replace(",", "."))
+            mem = float(fields[9].replace(",", "."))
+
+            if command != "top":
+                command_list.append((command, cpu, mem))
+        except BaseException:
+            pass
+    command_dict = {}
+    for command, cpu, mem in command_list:
+        if command in command_dict:
+            command_dict[command][0] += cpu
+            command_dict[command][1] += mem
+        else:
+            command_dict[command] = [cpu + 1, mem + 1]
+
+    resource_dict = {}
+
+    for command, [cpu, mem] in command_dict.items():
+        resource_dict[command] = (cpu ** 2 + mem ** 2) ** 0.5
+
+    width, height = None, None
+    try:
+        width, height = ((popen("xrandr | grep '*'").read()).split()[0]).split("x")
+        width = int(width)
+        height = int(height)
+    except BaseException:
+        pass
+    if not width or not height:
+        width = int(config['width'])
+        height = int(config['height'])
+    background = config['background']
+    margin = int(config['margin'])
+
+    cloud = WordCloud(
+        background_color=background,
+        width=width - 2 * int(margin),
+        height=height - 2 * int(margin)
+    ).generate_from_frequencies(resource_dict)
+
+    cloud.to_file("cloud.png")
     await context.edit("Uploading image . . .")
     await context.client.send_file(
         context.chat_id,
--- pagermaid/utils.py
+++ pagermaid/utils.py
@@ -1,11 +1,10 @@
 """ Libraries for python modules. """
 
-from os import remove, popen
+from os import remove
 from os.path import exists
 from emoji import get_emoji_regexp
 from requests import head
 from requests.exceptions import MissingSchema, InvalidURL, ConnectionError
-from wordcloud import WordCloud
 from urllib import request, parse
 from PIL import Image
 from math import floor, ceil
@@ -21,10 +20,9 @@ from telethon.tl.types import MessageEntityMentionName
 from pytz import timezone, country_timezones
 from asyncio import create_subprocess_shell
 from asyncio.subprocess import PIPE
-from pathlib import Path
 from collections import deque
 from youtube_dl import YoutubeDL
-from pagermaid import config, redis, working_dir, bot
+from pagermaid import redis, working_dir, bot
 
 
 async def upload_attachment(file_path, chat_id, reply_id, caption=None):
@@ -64,64 +62,6 @@ async def fetch_youtube_audio(context, url, reply_id):
         return
     await context.delete()
     remove("output.mp3")
-
-
-async def make_top_cloud(context):
-    await context.edit("Generating image . . .")
-    command_list = []
-    if not Path('/usr/bin/top').is_symlink():
-        output = str(await execute("top -b -n 1")).split("\n")[7:]
-    else:
-        output = str(await execute("top -b -n 1")).split("\n")[4:]
-    for line in output[:-1]:
-        line = sub(r'\s+', ' ', line).strip()
-        fields = line.split(" ")
-        try:
-            if fields[11].count("/") > 0:
-                command = fields[11].split("/")[0]
-            else:
-                command = fields[11]
-
-            cpu = float(fields[8].replace(",", "."))
-            mem = float(fields[9].replace(",", "."))
-
-            if command != "top":
-                command_list.append((command, cpu, mem))
-        except BaseException:
-            pass
-    command_dict = {}
-    for command, cpu, mem in command_list:
-        if command in command_dict:
-            command_dict[command][0] += cpu
-            command_dict[command][1] += mem
-        else:
-            command_dict[command] = [cpu + 1, mem + 1]
-
-    resource_dict = {}
-
-    for command, [cpu, mem] in command_dict.items():
-        resource_dict[command] = (cpu ** 2 + mem ** 2) ** 0.5
-
-    width, height = None, None
-    try:
-        width, height = ((popen("xrandr | grep '*'").read()).split()[0]).split("x")
-        width = int(width)
-        height = int(height)
-    except BaseException:
-        pass
-    if not width or not height:
-        width = int(config['width'])
-        height = int(config['height'])
-    background = config['background']
-    margin = int(config['margin'])
-
-    cloud = WordCloud(
-        background_color=background,
-        width=width - 2 * int(margin),
-        height=height - 2 * int(margin)
-    ).generate_from_frequencies(resource_dict)
-
-    cloud.to_file("cloud.png")
 
 
 async def upload_sticker(animated, bot, message, context, file, conversation):
